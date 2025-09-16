66369738d6840634981cad1fa9c024ce0aa4aa5c
Mitchell Gilmore <mgilmore@relace.ai>
2025-09-16 19:21:08 +0000

    add dropout and router overhead

--- megatron/training/theoretical_memory_usage.py
+++ megatron/training/theoretical_memory_usage.py
@@ -381,7 +381,7 @@ def compute_activation_memory(args, num_microbatches, verbose=False):
 
     # Memory footprint for moe transformer layer (self-attention and MLP).
     moe_activation_memory = (args.seq_length * args.micro_batch_size) * (
-        15 * args.hidden_size + args.moe_router_topk * (4 * args.moe_ffn_hidden_size + 2 * args.hidden_size)
+        17 * args.hidden_size + args.moe_router_topk * (5 * args.moe_ffn_hidden_size + 2 * args.hidden_size)
     )
     if verbose:
         print(
